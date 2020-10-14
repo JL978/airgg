ea7985f420e0ba3985ec5d6d0a6dd7b3e5983a92
Jimmy Lam <JimmyLam978@gmail.com>
2020-10-14 14:40:22 -0400

    done product page

--- src/components/product-page/ProductPage.js
+++ src/components/product-page/ProductPage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ProductPage() {
 	return (
-		<div className="ProductPageWrapper">
+		<div className="productPageWrapper">
 			<div className="info">
 				<div className="imgWrapper"></div>
 				<div className="description">
@@ -21,9 +21,29 @@ export default function ProductPage() {
 					<h2>Price: $50</h2>
 				</div>
 				<div className="vendor">
-					<div className="delivery"></div>
-					<div className="cta"></div>
-					<div className="info"></div>
+					<div className="delivery">
+						<h2>Delivery Time</h2>
+						<p>Seller Average</p>
+						<p>Guaranteed</p>
+					</div>
+					<div className="cta">
+						<button>Purchase</button>
+						<small>You won't be charged yet</small>
+					</div>
+					<div className="other">
+						<div className="line">
+							<p>Shop</p>
+							<p>TOMMY</p>
+						</div>
+						<div className="line">
+							<p>Stock</p>
+							<p>1</p>
+						</div>
+						<div className="line">
+							<p>Feedback</p>
+							<p>0</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
